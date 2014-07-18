c92ddb493e5419a2d45723232505b36cee1175ee
ftpaul <ftpaul@gmail.com>
2014-07-18 20:16:53 +0100

    more small improves

--- Gemfile
+++ Gemfile
@@ -4,12 +4,15 @@ gem 'rails', '4.0.2'
 
 gem 'pg'
 
+gem 'annotate'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'slim-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+
+gem 'omniauth'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id           :integer          not null, primary key
+#  first_name   :string(255)
+#  last_name    :string(255)
+#  bio          :text
+#  location     :string(255)
+#  phone_number :string(255)
+#  country_code :string(255)
+#  languages    :string(255)
+#  birth_year   :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Person < ActiveRecord::Base
 end
