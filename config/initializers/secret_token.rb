1e4ee84b6c12b8ba34a059e264d957091bbba926
ftpaul <ftpaul@gmail.com>
2014-07-18 18:21:17 +0100

    rails \o/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moonlighting::Application.config.secret_key_base = '9f77025a3126b5a55b43e75b6c30c867c503df8d8b884193ff974559958b427597f74e9ae7e568da5d30ee33cec70ad30171e465c7ffbe41f538accbae31700c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moonlighting::Application.config.session_store :cookie_store, key: '_moonlighting_session'
